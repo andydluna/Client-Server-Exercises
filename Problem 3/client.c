8723ffe4e1d9939f209510c53584f6405b6c37ad
Andy De Luna <raidenshogun05@gmail.com>
2025-01-31 23:48:05 -0600

    Initial commit for Problem 3

---
+++ Problem 3/client.c
@@ -0,0 +1,50 @@
+#include <netinet/in.h> //structure for storing address information
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> //for socket APIs
+#include <sys/types.h>
+
+int main(int argc, char const* argv[])
+{
+    int sockD = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in servAddr;
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port
+        = htons(9001); // use some unused port number
+    servAddr.sin_addr.s_addr = INADDR_ANY;
+
+    int connectStatus
+        = connect(sockD, (struct sockaddr*)&servAddr,
+                  sizeof(servAddr));
+
+    if (connectStatus == -1) {
+        printf("Error...\n");
+    }
+
+    else {
+        const int MAX_LENGTH = 999;
+        int i = 0, count = 0;
+        char clMsg[MAX_LENGTH];
+
+        printf("Please enter a string of characters ending with # (up to %i):\n", MAX_LENGTH);
+        scanf("%c", &clMsg[0]);
+
+        while (clMsg[i] != '#') {
+            i++;
+
+            // Input size validation
+            if (i >= MAX_LENGTH) {
+                printf("Error...\n");
+                printf("Input is more than %i chars...\n", MAX_LENGTH);
+
+                return 1;
+            }
+
+            scanf("%c", &clMsg[i]);
+        }
+    }
+
+    return 0;
+}
